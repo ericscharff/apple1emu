2539856db1970214cfe40639f2bdf2e7697d5bf3
Eric Scharff <eric.scharff@gmail.com>
2022-01-11 18:23:39 -0700

    Add license to source files

--- src/main/java/a2em/SWTUI.java
+++ src/main/java/a2em/SWTUI.java
@@ -1,3 +1,11 @@
+/* Copyright (c) 2007-2022, Eric Scharff
+   Permission to use, copy, modify, and/or distribute this software
+   for any purpose with or without fee is hereby granted, provided
+   that the above copyright notice and this permission notice appear
+   in all copies.
+   There is NO WARRANTY for this software.  See license.txt for
+   details. */
+
 package a2em;
 
 import org.eclipse.swt.SWT;

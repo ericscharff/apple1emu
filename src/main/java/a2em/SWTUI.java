18a2cc2d3a3688095bd472fd5823548cb5d52e7b
Eric Scharff <eric.scharff@gmail.com>
2022-01-12 18:23:55 -0700

    Switch license.txt to LICENSE.txt

--- src/main/java/a2em/SWTUI.java
+++ src/main/java/a2em/SWTUI.java
@@ -3,7 +3,7 @@
    for any purpose with or without fee is hereby granted, provided
    that the above copyright notice and this permission notice appear
    in all copies.
-   There is NO WARRANTY for this software.  See license.txt for
+   There is NO WARRANTY for this software.  See LICENSE.txt for
    details. */
 
 package a2em;
